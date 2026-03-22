96068421e5319950f1a09d55980e367e3583714c
LessandroFreitas <lessandrof@gmail.com>
2026-03-22 19:42:44 -0300

    comentario alununoRoutes em autenticar

--- routes/alunoRoutes.js
+++ routes/alunoRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const alunoController = require("../controllers/alunoController.js");
 const { autenticar } = require("../middlewares/autenticar.js"); //add essa linha para importar o middleware de autenticação
 
-router.get("/aluno", autenticar, alunoController.obterTodosAlunos);
+router.get("/aluno", autenticar, alunoController.obterTodosAlunos); // coloquei para funionar a autenticação só nessa rota
 router.post("/aluno", alunoController.criarAluno);
 router.delete("/aluno/:id", alunoController.deletarAluno);
 router.put("/aluno/:id", alunoController.editarAluno);
